544c5c05f63f8f06310cc97c5e29d7d84837f162
vasiliy-klimkin <vasily.klimkin@gmail.com>
2020-06-04 11:22:47 -0400

    Server now runs

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
+const PORT = 8080;
 const express = require('express');
+const app = express();
+
+
+app.get('/', (req,res) => {
+    res.json({status: 'ok'});
+})
+
+
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
