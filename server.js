dbb516bdbd3c9e073c81f9a2570017bc798109c6
vasiliy-klimkin <vasily.klimkin@gmail.com>
2020-06-04 11:28:50 -0400

    Fixed PR review changes.

--- server.js
+++ server.js
@@ -3,9 +3,5 @@ const express = require('express');
 const app = express();
 
 
-app.get('/', (req,res) => {
-    res.json({status: 'ok'});
-})
-
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
